ca628cf832a1c0663268e4239658db596e1556aa
bodhisrikar <tsrikar187@gmail.com>
2020-11-08 12:11:00 -0500

    configuring keyboard after updating text

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         mainActivityBinding = ActivityMainBinding.inflate(layoutInflater)
         val root = mainActivityBinding.root
         setContentView(root)
+
+        mainActivityBinding.doneButton.setOnClickListener {
+            updateNicknameText()
+            hideKeyboard(it)
+        }
+    }
+
+    private fun updateNicknameText() {
+        mainActivityBinding.apply {
+            nicknameText.text = mainActivityBinding.nicknameEdit.text
+            nicknameText.visibility = View.VISIBLE
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+        }
+    }
+
+    private fun hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
